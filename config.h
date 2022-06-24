99485048460941c10df3b4570a3ddf1470e62ac3
Paranoid Wolf <ajuph9224@gmail.com>
2022-06-24 12:55:55 +0530

    float firefox save window

--- config.h
+++ config.h
@@ -26,9 +26,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class            instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
-	{ "Brave Browser",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class            instance    title               tags mask     isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,               0,            1,           -1 },
+	{ "Brave",          NULL,       "Save File",        1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
